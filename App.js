09c7a6a523a94e6cb7054e15c7f871a2d5a5b67a
crisj1985 <crisj1985@hotmail.com>
2020-06-24 23:57:17 -0500

    Navigation, screens

--- App.js
+++ App.js
@@ -9,11 +9,51 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { DefaultTheme, Provider as paperProvider} from 'react-native-paper';
+
+import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import  Inicio from './views/Inicio';
+import NuevoCliente from './views/NuevoCliente';
+import DetallesCliente from './views/DetallesCliente';
+import BarraSuperior from './components/ui/BarraSuperior';
+
+const Stack = createStackNavigator();
+const theme = {
+  ...DefaultTheme,
+  colors:{
+    ...DefaultTheme.colors,
+    primary:'#1774F2'
+  }
+};
 
 const App = () => {
-  return ( 
-    <></>
-  );
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Inicio"
+          screenOptions={{
+            headerStyle: { backgroundColor: theme.colors.primary },
+            headerTintColor: theme.colors.surface,
+            headerTitleStyle: { fontWeight: 'bold' },
+          }}
+        >
+          <Stack.Screen
+            name="Inicio"
+            component={Inicio}
+            options={({ navigation, route }) => ({
+              headerTitleAlign : 'center',
+              headerLeft: (props) => <BarraSuperior {...props} navigation={navigation} route={route} />,
+            })}
+          />
+          <Stack.Screen name="NuevoCliente" component={NuevoCliente} />
+          <Stack.Screen name="DetallesCliente" component={DetallesCliente} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  )
 };
 
 const styles = StyleSheet.create({});
---
+++ views/Inicio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+
+const Inicio =() => {
+
+    return (
+    <Text>Desde Inicio</Text>
+    );
+
+};
+
+export default Inicio;
